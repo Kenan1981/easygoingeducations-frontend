2eb416433eb5fa0700c456e8c26b78071f00c01c
yusuf <yusufs3435@gmail.com>
2024-10-29 19:40:36 +0100

    some changes

--- src/components/common/form-fields/index.js
+++ src/components/common/form-fields/index.js
@@ -1,5 +1,15 @@
 import { TextInput } from "./text-input";
 import { PasswordInput } from "./password-input";
 import { SubmitButton } from "./submit-button";
+import { DateInput } from "./date-input";
+import { SelectInput } from "./select-input";
+import { MaskedInput } from "./masked-input";
 
-export { TextInput, PasswordInput, SubmitButton }
+export {
+	TextInput,
+	PasswordInput,
+	SubmitButton,
+	DateInput,
+	SelectInput,
+	MaskedInput,
+};
--- src/components/dashboard/admin/admin-create-form.jsx
+++ src/components/dashboard/admin/admin-create-form.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+"use client";
+import { createAdminAction } from "@/actions/admin-actions";
+import {
+	DateInput,
+	MaskedInput,
+	SelectInput,
+	TextInput,
+} from "@/components/common/form-fields";
+import { config } from "@/helpers/config";
+import { initialResponse } from "@/helpers/form-validation";
+import React from "react";
+import { useFormState } from "react-dom";
 
 export const AdminCreateForm = () => {
-  return (
-    <div>AdminCreateForm</div>
-  )
-}
+	const [state, dispatch] = useFormState(createAdminAction, initialResponse);
+
+	return (
+		<form action={dispatch}>
+			<TextInput
+				name="name"
+				className="mb-3"
+				label="First name"
+				errorMessage={state?.errors?.name}
+			/>
+
+			<TextInput
+				name="surname"
+				className="mb-3"
+				label="Last name"
+				errorMessage={state?.errors?.surname}
+			/>
+
+			<SelectInput
+				name="gender"
+				className="mb-3"
+				label="Gender"
+				errorMessage={state?.errors?.gender}
+				options={config.genders}
+				optionLabel="label"
+				optionValue="value"
+			/>
+
+			<TextInput
+				type="date"
+				name="birthDay"
+				className="mb-3"
+				label="Date of borth"
+				errorMessage={state?.errors?.birthDay}
+			/>
+
+			<TextInput
+				name="birthPlace"
+				className="mb-3"
+				label="Place of birth"
+				errorMessage={state?.errors?.birthPlace}
+			/>
+
+			<MaskedInput
+				name="phoneNumber"
+				className="mb-3"
+				label="Phone number"
+        mask="999-999-9999"
+				errorMessage={state?.errors?.phoneNumber}
+			/>
+		</form>
+	);
+};
