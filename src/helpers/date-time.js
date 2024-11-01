b8e8d3887801a499fb1064da23c0a36f15b1ed4f
yusuf <yusufs3435@gmail.com>
2024-11-01 19:28:05 +0100

    some changes

--- src/helpers/date-time.js
+++ src/helpers/date-time.js
@@ -4,6 +4,10 @@ export const formatDatell = (date) => {
 	return moment(date).format("ll");
 };
 
+export const formatTimeLT = (date) => {
+	return moment(date, "HH:mm:ss").format("LT");
+};
+
 export const isLater = (timeBefore, timeAfter) => {
 	const tb = moment(timeBefore, "HH:mm");
 	const ta = moment(timeAfter, "HH:mm");
